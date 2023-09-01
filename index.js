45057c87532e6d4d4fe2c633757c3da3aaf004ff
wangyulong <wangyulong@kyland.com>
2023-09-01 16:46:03 +0800

    version

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const {Server} = require("socket.io");
-const io = new Server(server, {
+const socketio = require("socket.io");
+const io = socketio(server, {
     cors: {
         origin: "*"
     }
